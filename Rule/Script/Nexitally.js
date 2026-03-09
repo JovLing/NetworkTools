224c36e627914a7f869074324760e903d7cfb4f9
花以北 <90776145+JovLing@users.noreply.github.com>
2026-03-09 14:26:11 +0800

    Update Nexitally.js

--- Rule/Script/Nexitally.js
+++ Rule/Script/Nexitally.js
@@ -19,9 +19,9 @@ function main(config) {
         '*.msftncsi.com',
         '*.msftconnecttest.com',
         'time.*.com',
-		'time.*.gov'，
-        'time.*.edu.cn'，
-        'time.*.apple.com'，
+		'time.*.gov',
+        'time.*.edu.cn',
+        'time.*.apple.com',
         'ntp.*.com',
 		'stun.*.*'
       ],
@@ -34,7 +34,7 @@ function main(config) {
         'https://223.5.5.5/dns-query'
       ],
       'fallback': [
-        'https://8.8.8.8/dns-query'，
+        'https://8.8.8.8/dns-query',
         'https://1.1.1.1/dns-query'
       ],
       'fallback-filter': {
